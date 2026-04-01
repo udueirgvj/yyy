95a2c18e8e369e7b0ed06a0535a3c54c1822396c
udueirgvj <lllsgl8j@gmail.com>
2026-04-01 11:38:46 +0300

    Update AdminPanel.jsx

--- src/AdminPanel.jsx
+++ src/AdminPanel.jsx
@@ -644,7 +644,7 @@ function ImpostersManager({db}) {
     const botId="bot_"+userId;const nid=uidGen();
     await set(ref(db,"messages/"+botId+"/"+nid),{id:nid,chatId:botId,text:"🛡 رد فريق SDSF:
 
-"+txt.trim()+"
+text: txt.trim(),
 
 🔢 رقم البلاغ: "+rId,from:"bot_dfgfd_official",senderName:"SDSF",time:new Date().toLocaleTimeString("ar-SA",{hour:"2-digit",minute:"2-digit"}),type:"text",isOfficialBot:true,createdAt:Date.now()}).catch(()=>{});
     await update(ref(db,"imposterReports/"+rId),{status:"replied",reply:txt});
